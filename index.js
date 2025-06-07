54dfdc1eeb9afef3594c2d26aca42b6214655735
Khen chandarapisey <piseykhenchandara93@gmail.com>
2025-06-07 18:09:12 +0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/hello", (req,res) => {
-    res.send('hell ooo vv')
+    res.send('hell vv')
 });
 app.listen(PORT, () => {
     console.log("backend is runing on port 5000")
